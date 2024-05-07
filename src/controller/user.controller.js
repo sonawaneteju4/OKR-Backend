aadd9cc3471dde8de1c97dea2831e353f2245777
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-05-07 15:56:15 +0530

    smg

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -6,50 +6,104 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
-const generateAccessAndRefereshTokens = async(userId) =>{
+const generateAccessAndRefereshTokens = async (userId) => {
   try {
-      const user = await User.findById(userId)
-      const accessToken = user.generateAccessToken()
-      const refreshToken = user.generateRefreshToken()
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
 
-      user.refreshToken = refreshToken
-      await user.save({ validateBeforeSave: false })
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
 
-      return {accessToken, refreshToken}
-
-
+    return { accessToken, refreshToken };
   } catch (error) {
-      throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    throw new ApiError(
+      500,
+      "Something went wrong while generating referesh and access token"
+    );
   }
-}
-
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, phone } = req.body;
   if ([name, email, password, phone].some((field) => field?.trim() === "")) {
-    throw new ApiError(400, "All fields are required")
+    throw new ApiError(400, "All fields are required");
   }
-    let user = await User.findOne({ email });
-    if(user){  
-      throw new ApiError(400, "Sorry a User with this email already Exist")
-    }
-    user = await User.create({
-      name : req.body.name,
-      email : req.body.email,
-      password : req.body.password,
-      phone : req.body.phone
-    })
-   
-    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+  let user = await User.findOne({ email });
+  if (user) {
+    throw new ApiError(400, "Sorry a User with this email already Exist");
+  }
+  user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    phone: req.body.phone,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
   if (!createdUser) {
-      throw new ApiError(500, "Something went wrong while registering the user")
+    throw new ApiError(500, "Something went wrong while registering the user");
   }
 
-  return res.status(201).json(
-      new ApiResponse(200, createdUser, "User registered Successfully")
-  )
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export { registerUser ,generateAccessAndRefereshTokens};
+const loginUser = asyncHandler(async (req, res) => {
+
+  const { email, password } = req.body;
+  console.log(email);
+
+  if ( !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({
+   email
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+export { registerUser, generateAccessAndRefereshTokens, loginUser};
