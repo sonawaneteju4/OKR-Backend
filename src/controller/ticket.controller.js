cbe39189beeef298eb37400a99bc8a44117c24f9
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-05-16 18:05:56 +0530

    update Ticket

--- src/controller/ticket.controller.js
+++ src/controller/ticket.controller.js
@@ -11,17 +11,8 @@ const getAllTickets = asyncHandler(async (req, res) => {
   const { email, priority, status, category, issue, resolution, expectedTime } =
     req.query;
   try {
-    const tickets = await Ticket.find({
-      email,
-      priority,
-      status,
-      category,
-      issue,
-      resolution,
-      expectedTime,
-    }).sort({ createdAt: -1 });
+    const tickets = await Ticket.find({}).sort({ $natural: -1 });
     res.status(200).json(tickets);
-    console.log(tickets)
   } catch (error) {
     throw new ApiError(500, error.message);
   }
@@ -52,20 +43,43 @@ const createTicket = asyncHandler(async (req, res) => {
     const saveTicket = await ticket.save();
     res.status(200).json(saveTicket);
   } catch (error) {
-    throw new ApiError(500, error.message = "Something Went Wrong");
+    throw new ApiError(500, (error.message = "Something Went Wrong"));
   }
 });
 
 // @desc    Get ticket by id
 
-
+const getTicketById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid ticket id");
+  }
+  try {
+    const ticket = await Ticket.findById(id);
+    res.status(200).json(ticket);
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
 
 // @desc    Update ticket
-
-
+const updateTicket = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw new ApiError(400, "Invalid ticket id");
+  }
+  try {
+    const ticket = await Ticket.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(ticket);
+  } catch (error) {
+    throw new ApiError(500, error.message);
+  }
+});
 
 // @desc    Close ticket
 
 
 
-export { getAllTickets, createTicket };
+export { getAllTickets, createTicket, getTicketById, updateTicket };
--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -1,8 +1,12 @@
 import {Router} from 'express'
-import {getAllTickets, createTicket} from '../controller/ticket.controller.js'
+import {getAllTickets, createTicket, getTicketById, updateTicket} from '../controller/ticket.controller.js'
 
 const router = Router()
 router.route('/getalltickets').get(getAllTickets)
 router.route('/createnewticket').post(createTicket)
+router.route('/getTicketById/:id').get(getTicketById)
+router.route('/updateTicket/:id').get(updateTicket)
+
+
 
 export default router;
